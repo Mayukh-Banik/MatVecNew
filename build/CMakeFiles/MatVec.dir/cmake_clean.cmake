d16f93cd8a748e75491949559019b8e5ebac040f
Mayukh Banik <mayukh.banik@stonybrook.edu>
2024-08-12 21:04:15 -0400

    Starting

---
+++ build/CMakeFiles/MatVec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatVec.dir/example.cpp.o"
+  "CMakeFiles/MatVec.dir/example.cpp.o.d"
+  "MatVec.pdb"
+  "MatVec.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatVec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
